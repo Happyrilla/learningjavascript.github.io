0b74bda9162c391eade17757d371f7e499dad388
happyrilla <basukabomberumpe@gmail.com>
2026-06-06 17:24:30 +0200

    test

--- video.js
+++ video.js
@@ -3,8 +3,10 @@ const video = document.querySelector(".example")
 
 function toggle() {
     if (video.paused) {
-        video.play
+        video.play()
     } else {
-        video.play
+        video.pause()
     }
 }
+
+btn.onclick("click", toggle)
